a8521e1ff700f39c2b08c630ecd796da4ef73ef1
BruceF <felicity.bruce@vodafone.com>
2023-05-01 14:05:55 +0100

    basic viewholder - home frag shows database users

--- app/src/main/java/com/example/axiom/HomeFragment.kt
+++ app/src/main/java/com/example/axiom/HomeFragment.kt
@@ -5,16 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class HomeFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PostListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        recyclerView = view.findViewById(R.id.recyclerView)
+        adapter = PostListAdapter(mutableListOf()) // pass the list of entities to the adapter here
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // observe the LiveData from the ViewModel and update the adapter accordingly
+        val viewModel = ViewModelProvider(this)[AxiomViewModel::class.java]
+        viewModel.allEntities.observe(viewLifecycleOwner) { entities ->
+            for (entity in entities) {
+                if (!adapter.entities.contains(entity)) {
+                    adapter.addEntity(entity)
+                } else {
+                    adapter.updateEntity(entity)
+                }
+            }
+            val entitiesToRemove = adapter.entities.filter { entity ->
+                !entities.contains(entity)
+            }
+            for (entityToRemove in entitiesToRemove) {
+                adapter.removeEntity(entityToRemove)
+            }
+        }
+
+        return view
+    }
 }
